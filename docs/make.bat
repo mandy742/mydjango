96884e5bbfc9b1f0df601ac2d6ade9462442bd7e
mandy <mchipepera2015@gmail.com>
2024-09-06 11:29:49 +0200

    Added docstrings

--- docs/make.bat
+++ docs/make.bat
@@ -4,12 +4,15 @@ pushd %~dp0
 
 REM Command file for Sphinx documentation
 
+REM Check if SPHINXBUILD environment variable is set, if not, set it to 'sphinx-build'
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
+REM Set the source and build directories
 set SOURCEDIR=.
 set BUILDDIR=_build
 
+REM Check if sphinx-build command is available
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
@@ -22,12 +25,13 @@ if errorlevel 9009 (
 	echo.https://www.sphinx-doc.org/
 	exit /b 1
 )
-
+REM If no arguments are provided, show help
 if "%1" == "" goto help
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
+REM Show help message
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
